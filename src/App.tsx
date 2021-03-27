f063768641b8fc5c79a67e76841483739d71b97b
Arlei F. Farnetani Junior <farnetani@gmail.com>
2021-03-27 00:24:06 -0300

    desafio finalizado

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,68 @@
-import { TaskList } from './components/TaskList'
-import { Header } from './components/Header'
+import { useEffect, useState } from 'react'
+import { api } from './services/api'
+
+import { SideBar } from './components/SideBar'
+import { Content } from './components/Content'
+
 import './styles/global.scss'
 
+import './styles/sidebar.scss'
+import './styles/content.scss'
+
+interface GenreResponseProps {
+  id: number
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family'
+  title: string
+}
+
+interface MovieProps {
+  imdbID: number
+  Title: string
+  Poster: string
+  Ratings: Array<{
+    Source: string
+    Value: string
+  }>
+  Runtime: string
+}
+
 export function App() {
+  const [genres, setGenres] = useState<GenreResponseProps[]>([])
+  const [selectedGenreId, setSelectedGenreId] = useState(1)
+  const [movies, setMovies] = useState<MovieProps[]>([])
+  const [selectedGenre, setSelectedGenre] = useState<GenreResponseProps>(
+    {} as GenreResponseProps
+  )
+
+  useEffect(() => {
+    api.get<GenreResponseProps[]>('genres').then((response) => {
+      setGenres(response.data)
+    })
+  }, [])
+
+  useEffect(() => {
+    api
+      .get<MovieProps[]>(`movies/?Genre_id=${selectedGenreId}`)
+      .then((response) => {
+        setMovies(response.data)
+      })
+
+    api
+      .get<GenreResponseProps>(`genres/${selectedGenreId}`)
+      .then((response) => {
+        setSelectedGenre(response.data)
+      })
+  }, [selectedGenreId])
+
   return (
-    <>
-      <Header />
-      <TaskList />
-    </>
+    <div style={{ display: 'flex', flexDirection: 'row' }}>
+      <SideBar
+        genres={genres}
+        selectedGenreId={selectedGenreId}
+        setSelectedGenreId={setSelectedGenreId}
+      />
+
+      <Content movies={movies} selectedGenre={selectedGenre} />
+    </div>
   )
 }
